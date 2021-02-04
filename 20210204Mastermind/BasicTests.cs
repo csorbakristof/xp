38e96252e7f45d4b67c53083e8a8a71d2657c448
Kristof <kristof@aut.bme.hu>
2021-02-04 11:59:47 +0100

    MasterMind HidegAttilaval

--- 20210204Mastermind/BasicTests.cs
+++ 20210204Mastermind/BasicTests.cs
@@ -1,4 +1,5 @@
 using System;
+using MasterMindCommon;
 using Xunit;
 
 namespace _20210204Mastermind
@@ -10,7 +11,31 @@ namespace _20210204Mastermind
         [Fact]
         public void Instantiation()
         {
-            (int black, int white) = mm.Eval("");
+            var result = mm.Eval("", "");
+            Assert.Equal((0, 0), result);
+        }
+
+        [Theory]
+        [InlineData("AAAA", "AAAA", 4, 0)]
+        [InlineData("AAAB", "AAAA", 3, 0)]
+        [InlineData("ABBB", "CCCA", 0, 1)]
+        [InlineData("ABCD", "BEEA", 0, 2)]
+        [InlineData("ABCD", "ABDC", 2, 2)]
+        public void EvalTests(string correct, string guess, int black, int white)
+        {
+            var result = mm.Eval(correct, guess);
+            Assert.Equal((black, white), result);
+        }
+
+        [Fact]
+        public void TestGame()
+        {
+            var correct = "ABCD";
+            Assert.Equal((0, 2), mm.Eval("BEAF", correct));
+            Assert.Equal((1, 2), mm.Eval("BEAD", correct));
+            Assert.Equal((2, 1), mm.Eval("BECD", correct));
+            Assert.Equal((2, 2), mm.Eval("BACD", correct));
+            Assert.Equal((4, 0), mm.Eval("ABCD", correct));
         }
     }
 }
---
+++ MasterMindCommon/MasterMind.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MasterMindCommon
+{
+    public class MasterMind
+    {
+        public (int black, int white) Eval(string correct, string g)
+        {
+            var c = correct.ToCharArray();
+            if (g == correct) return (g.Length, 0);
+            int black = 0;
+            int white = 0;
+            for (int i = 0; i < g.Length; i++)
+            {
+                if (g[i] == correct[i])
+                {
+                    black++;
+                    c[i] = '-';
+                }
+            }
+            for (int i = 0; i < g.Length; i++)
+            {
+                if (c.Contains(g[i]))
+                {
+                    white++;
+                }
+            }
+
+            return (black, white);
+        }
+    }
+}
